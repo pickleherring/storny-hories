c9c21b64a17d4f0e6618f1fbef86ae9e376b5591
pickleherring <100018622+pickleherring@users.noreply.github.com>
2026-02-27 18:32:14 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ import stoat
 from handlers import hf
 
 
-# logger = logging.getLogger("storny_hories")
+logger = logging.getLogger("storny_hories")
 
 dotenv.load_dotenv()
-STOAT_TOKEN = os.environ['STOAT_TOKEN']
-HEALTHCHECK_CHANNEL_ID = os.environ['STOAT_HEALTHCHECK_CHANNEL_ID']
+STOAT_TOKEN = os.environ["STOAT_TOKEN"]
+HEALTHCHECK_CHANNEL_ID = os.environ["STOAT_HEALTHCHECK_CHANNEL_ID"]
 
 url_handlers = (
     hf.HFHandler(),
@@ -32,17 +32,14 @@ class Client(stoat.Client):
         await channel.send("healthcheck")
         await asyncio.sleep(2)
         if not self.healthpulse:
-            # logger.warning("Failed health check")
-            print("Failed health check")
+            logger.warning("Failed health check")
             await self.close()
         else:
-            # logger.info("Passed health check")
-            print("Passed health check")
+            logger.info("Passed health check")
 
     async def on_ready(self, event: stoat.ReadyEvent, /) -> None:
         for server in event.servers:
-            # logger.info(f"joined '{server.name}'")
-            print(f"joined '{server.name}'")
+            logger.info(f"joined '{server.name}'")
         await self.healthcheck()
     
     async def on_message(self, message: stoat.Message, /) -> None:
@@ -50,19 +47,13 @@ class Client(stoat.Client):
             if message.channel.id == HEALTHCHECK_CHANNEL_ID:
                 self.healthpulse = True
             return
-        
         for url_handler in url_handlers:
-            try:
-                embeds = await url_handler.message_to_embeds(message.content)
-                if embeds:
-                    await message.channel.send(embeds=embeds)
-            except Exception as e:
-                # logger.error(repr(e))
-                print(repr(e))
+            embeds = await url_handler.message_to_embeds(message.content)
+            if embeds:
+                await message.channel.send(embeds=embeds)
 
 
-# logger.info("starting")
-print("starting")
+logger.info("starting")
 
 
 while True:
@@ -71,7 +62,7 @@ while True:
         client.run(
             STOAT_TOKEN,
             log_level=logging.DEBUG,
-            asyncio_debug=True
+            root_logger=True,
         )
     except KeyboardInterrupt:
         break
